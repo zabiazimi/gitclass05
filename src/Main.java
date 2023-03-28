63bc124d64736bcba713fd7ec181db9c2300c801
zabiazimi <zabiazimi1994@gmail.com>
2023-03-28 16:38:07 -0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
             System.out.println(i);
 
         }
